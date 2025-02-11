be970baca0ec5234b10b349aae6ff3f698d32c00
Routund <routundity@gmail.com>
2025-02-12 08:53:46 +1300

    Added Sphere

--- UvSphere.gd
+++ UvSphere.gd
@@ -25,50 +25,6 @@ func _ready():
 	randomize()
 	surface_array.resize(Mesh.ARRAY_MAX)
 
-	# Vertex indices.
-	var thisrow = 0
-	var prevrow = 0
-	var point = 0
-
-	# Loop over rings.
-	for i in range(rings + 1):
-		var v = float(i) / rings
-		var w = sin(PI * v)
-		var y = cos(PI * v)
-
-		# Loop over segments in ring.
-		for j in range(radial_segments + 1):
-			var u = float(j) / radial_segments
-			var x = sin(u * PI * 2.0)
-			var z = cos(u * PI * 2.0)
-			var vert = Vector3(x * radius * w, y * radius, z * radius * w)
-			verts.append(vert)
-			normals.append(vert.normalized())
-			uvs.append(Vector2(u, v))
-			point += 1
-
-			# Create triangles in ring using indices.
-			if i > 0 and j > 0:
-				indices.append(prevrow + j - 1)
-				indices.append(prevrow + j)
-				indices.append(thisrow + j - 1)
-
-				indices.append(prevrow + j)
-				indices.append(thisrow + j)
-				indices.append(thisrow + j - 1)
-
-		prevrow = thisrow
-		thisrow = point
-	# Assign arrays to surface array.
-	surface_array[Mesh.ARRAY_VERTEX] = verts
-	surface_array[Mesh.ARRAY_TEX_UV] = uvs
-	surface_array[Mesh.ARRAY_NORMAL] = normals
-	surface_array[Mesh.ARRAY_INDEX] = indices
-
-	# Create mesh surface from mesh array.
-	# No blendshapes, lods, or compression used.
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
-
 func add_impact(impact_site : Vector3):
 	impact_site = impact_site.normalized()
 	impact_points.append([impact_site,k])
@@ -86,20 +42,13 @@ func _process(delta : float) -> void:
 		if len(impact_points)==15:
 			impact_points.pop_at(0)
 	
-	recalculate_positions()
-	
-	surface_array[Mesh.ARRAY_VERTEX] = verts
-	
-	mesh.clear_surfaces()
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
-	
 	if Input.is_action_pressed("left"):
 		rotation.y -= 0.02
 	elif Input.is_action_pressed("right"):
 		rotation.y += 0.02
 
-func recalculate_positions():
-	pass
+#func recalculate_positions():
+	#pass
 	#if len(impact_points) != 0:
 		#for i in range(len(verts)):
 			#var vec_normal : Vector3 = verts[i].normalized()
