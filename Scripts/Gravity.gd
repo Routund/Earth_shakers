a370c965d95cdcb56dbadc3f818d4abefbf597d2
Routund <routundity@gmail.com>
2025-02-12 19:45:14 +1300

    Worked more on gravity

--- Scripts/Gravity.gd
+++ Scripts/Gravity.gd
@@ -16,7 +16,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func check_ground(place : Vector3):
+func gravitate(position : Vector3):
+	var gravity_magnitude = 1.0
+	var target_position : Vector3 = position -position.normalized() * gravity_magnitude
+	target_position = check_ground(target_position)
+	return target_position
+
+func check_ground(place : Vector3) -> Vector3:
 	var impact_points : Array = $Planet.impact_points
 	var k : float = $Planet.k
 	
@@ -28,6 +34,10 @@ func check_ground(place : Vector3):
 		var angle = acos(vec_normal.dot(impact_point[0]))
 		var x = abs(angle -  k + impact_point[1])
 		additive -= 0.1 * pow(e,-dampening * x) * (cos(frequency * x + phase_shift) + sin(frequency*x + phase_shift))
-	
 	var dist_from_center = radius + additive
 	var place_dist = place.length()
+	
+	if dist_from_center > place_dist:
+		return vec_normal * (radius + additive)
+	else:
+		return place
