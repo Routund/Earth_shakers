ed41889d5fe32689e8799d353cf64f58fd2b490d
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-02-13 10:24:46 +1300

    wholesome :)

--- player.gd
+++ player.gd
@@ -25,8 +25,8 @@ func _physics_process(delta: float) -> void:
 		$RayCast3D.target_position = accel * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity += JUMP_VELOCITY* position.normalized()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
