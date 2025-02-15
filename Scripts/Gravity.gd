25210dbff887d15a6b6c9d0bcf7f7c3441e2b832
Routund <routundity@gmail.com>
2025-02-15 13:16:28 +1300

    Added jumping and collision to planet

--- Scripts/Gravity.gd
+++ Scripts/Gravity.gd
@@ -4,11 +4,12 @@ const e : float = 2.718281;
 const dampening : float = 2.0;
 const phase_shift : float = PI/4.0;
 const frequency : float = 15.0;
-const height : float = 0.1;
-const radius : float = 1.5;
+const height : float = 0.09;
+const radius : float = 2.0;
 
 var impact_points = []
 var k = 0
+var index = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,13 +21,11 @@ func _process(delta: float) -> void:
 	pass
 
 func gravitate(position : Vector3):
-	var gravity_magnitude = 1.0
+	var gravity_magnitude = 5.0
 	var target_position : Vector3 = position.normalized() * (position.length() - gravity_magnitude)
-	target_position = check_ground(target_position)
-	
 	return [position - target_position,target_position]
 
-func check_ground(place : Vector3) -> Vector3:
+func check_ground(place : Vector3):
 	
 	var vec_normal : Vector3 = place.normalized()
 	var additive : float = 0
@@ -40,7 +39,6 @@ func check_ground(place : Vector3) -> Vector3:
 	var place_dist = place.length()
 	
 	if dist_from_center > place_dist:
-		print("stopped %f" % k)
-		return vec_normal * (radius + additive)
+		return [vec_normal * (radius + additive),true]
 	else:
-		return place
+		return [vec_normal * (radius + additive),false]
--- player.gd
+++ player.gd
@@ -2,11 +2,16 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 const SENS = 0.005
+
+var gravitational_velocity : Vector3 = Vector3(0,0,0)
+var jump_velocity : Vector3 = Vector3(0,0,0)
 
 @onready var head = $head
 @onready var camera = $head/Camera3D
+
+var grounded : bool = false
+var perpendicular_movement : Vector3 = Vector3(0,0,0)
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -17,16 +22,27 @@ func _ready():
 		#camera.rotate_x(-event.relative.y * SENS)
 		#camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-85), deg_to_rad(85))
 		#
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if !grounded:
 		var accel = Gravity.gravitate(position)
-		velocity = -accel[0] * delta * 50
-		$RayCast3D.target_position = accel[1] - position
+		gravitational_velocity += -accel[0] * delta * 1
+		var ground_result = Gravity.check_ground(position + velocity * delta)
+		if ground_result[1]:
+			gravitational_velocity = Vector3.ZERO
+			position = ground_result[0]
+			grounded = true
+			jump_velocity = Vector3(0,0,0)
+			velocity = Vector3.ZERO
+		$RayCast3D.target_position = gravitational_velocity *delta
+	else:
+		position = Gravity.check_ground(position + velocity*delta)[0]
+		print(position)
+		$RayCast3D.target_position = gravitational_velocity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity += JUMP_VELOCITY* position.normalized()
+
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
@@ -35,10 +51,17 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		perpendicular_movement.x = direction.x * SPEED
+		perpendicular_movement.z = direction.z * SPEED
 	else:
-		velocity.x += move_toward(velocity.x, 0, SPEED)
-		velocity.z += move_toward(velocity.z, 0, SPEED)
-
+		perpendicular_movement.x = move_toward(perpendicular_movement.x, 0, SPEED)
+		perpendicular_movement.z = move_toward(perpendicular_movement.z, 0, SPEED)
+	
+	if Input.is_action_just_pressed("jump") and grounded:
+		jump_velocity += position.normalized() * 5
+		print("Jumped")
+		grounded = false
 	move_and_slide()
+	
+	velocity = perpendicular_movement + gravitational_velocity + jump_velocity
+	
