804477ed7fa81b24f91edfacd839563d7a51338d
Routund <routundity@gmail.com>
2025-02-25 09:47:40 +1300

    Made ripples coloured and made more things shake

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 var damage = 1
-const SPEED = 20
+const SPEED = 30
 var gravity_scale = 0.025
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- Scripts/ship.gd
+++ Scripts/ship.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
-var health = 25
-var speed = 50
+var health = 5
+var speed = 15
 var velocity = Vector3(0,0,0)
 
 # Called when the node enters the scene tree for the first time.
@@ -15,3 +15,11 @@ func _process(delta: float) -> void:
 	transform = Gravity.rotate_object(position,transform)
 	velocity = speed * -transform.basis.x
 	position += velocity * delta
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.is_in_group('bullet'):
+		self.health -= area.get_parent().damage
+		area.get_parent().queue_free()
+		if self.health <= 0:
+			queue_free()
+	pass # Replace with function body.
